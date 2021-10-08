bd9ef3a2d228cba68610a18281dce328d9bf24a0
blue-putty <65861156+blue-putty@users.noreply.github.com>
2021-10-07 21:07:40 -0400

    remove comments

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,12 @@
-(() => {
+function getAnswerKeys() {
   const questions = document.getElementsByClassName(
     "freebirdFormeditorViewItemContentWrapper"
   );
   for (const question of questions) {
     question.click();
-    console.log(question);
     const [keyButton] = question.querySelectorAll(
       '[data-tooltip="Answer key and points"]'
     );
-    console.log(keyButton);
     keyButton.click();
-    // break;
   }
-})();
+}
