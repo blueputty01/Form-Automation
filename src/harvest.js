0d89d0466c3f32ae762744db6b96051c5b0bb1db
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-02-13 12:12:23 -0500

    fix keyforkey error

--- src/harvest.js
+++ src/harvest.js
@@ -2,7 +2,7 @@
 import Wizard from './components/Wizard';
 
 const wiz = new Wizard({ clear: true });
-const keyForKey = ['A', 'B', 'C', 'D'];
+const keyForKey = 'ABCD';
 wiz.fill({ offset: 0, keyForKey });
 wiz.fill({ offset: 8 * 4, keyForKey });
 wiz.fill({ offset: (8 + 16) * 4, keyForKey });
