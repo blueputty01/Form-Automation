96e497395fe563b8917bf63a7b5d7ad7c64fc08c
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-02-25 22:20:58 -0500

    match form emoji with output

--- src/hsk4.js
+++ src/hsk4.js
@@ -1,22 +1,26 @@
 // @ts-nocheck
 import Wizard from './components/Wizard';
 
-const wiz = new Wizard({ clear: true });
-let offset = 0;
+const onClear = () => {
+  let offset = 0;
 
-let keyForKey = '✅❎';
-wiz.fill({ offset, keyForKey });
-offset += 2 * 10;
+  let keyForKey = '✔❌';
+  wiz.fill({ offset, keyForKey });
+  offset += 2 * 10;
 
-keyForKey = 'ABCD';
-wiz.fill({ offset, keyForKey });
-offset += 4 * 15;
+  keyForKey = 'ABCD';
+  wiz.fill({ offset, keyForKey });
+  offset += 4 * 15;
 
-wiz.fill({ offset, keyForKey });
-offset += 4 * 20;
+  wiz.fill({ offset, keyForKey });
+  offset += 4 * 20;
 
-keyForKey = 'ABCDEF';
-wiz.fill({ offset, keyForKey });
+  keyForKey = 'ABCDEF';
+  wiz.fill({ offset, keyForKey });
 
-offset = 220;
-wiz.fill({ offset, keyForKey });
+  keyForKey = 'ABCD';
+  offset = 220;
+  wiz.fill({ offset, keyForKey });
+};
+
+const wiz = new Wizard({ clear: true, onClear });
