8bff54bd6d1884675b59e3413f1ed1f87065549c
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-03-06 11:53:46 -0500

    add simple conversion skip google form

---
+++ src/convert.js
@@ -0,0 +1,6 @@
+let key = prompt('Paste answer key:');
+
+//replace spaces
+key = key.replaceAll(' ', '	');
+
+console.log(key);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const BookmarkletPlugin = require('./webpack/BookmarkletPlugin');
 
-const fileNames = ['free', 'strive', 'harvest', 'hsk4', 'hsk5'];
+const fileNames = ['free', 'strive', 'harvest', 'hsk4', 'hsk5', 'convert'];
 const files = fileNames.reduce((prev, curr) => {
   prev[curr] = `./src/${curr}.js`;
   return prev;
