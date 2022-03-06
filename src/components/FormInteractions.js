23fd0a531cbb800dbfd970e68157ee01eb1a9ab4
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-03-06 10:26:01 -0500

    update selectors
    match google's hashed class names

--- src/components/FormInteractions.js
+++ src/components/FormInteractions.js
@@ -1,11 +1,19 @@
 // @ts-nocheck
+const mcqClass = 'P93BCe';
+const checkedClass = 'i9xfbb N2RpBe';
+const inputSelector = '[aria-label="Add a correct answer"]';
+
+const questionClass = 'gk8i6';
+const answerKeyButtonSelector = '[data-tooltip="Answer key and points"]';
+
 class FormInteractions {
+  checkboxes;
+
+  constructor() {
+    this.checkboxes = Array.from(document.getElementsByClassName(mcqClass));
+  }
+
   inputKey = (choices, offset, key) => {
-    const checkboxes = Array.from(
-      document.getElementsByClassName(
-        'quantumWizTogglePaperradioRadioContainer'
-      )
-    );
     console.log(choices, offset, key);
     for (
       let answerIndex = 0, checkboxIndex = offset;
@@ -14,50 +22,42 @@ class FormInteractions {
     ) {
       const answer = key[answerIndex];
       checkboxIndex += answer;
-      checkboxes[checkboxIndex].click();
+      this.checkboxes[checkboxIndex].click();
       checkboxIndex += choices - answer;
     }
   };
 
   setShortAnswer = (correct) => {
-    let inputs = document.querySelectorAll(
-      '[aria-label="Add a correct answer"]'
-    );
+    let inputs = document.querySelectorAll(inputSelector);
     // for (let i = 0; i < correct.length; i++) {
     inputs[0].click();
     // }
   };
 
   showAnswerKeys = () => {
-    const questions = document.getElementsByClassName(
-      'freebirdFormeditorViewItemContentWrapper'
+    const questions = Array.from(
+      document.getElementsByClassName(questionClass)
     );
-    for (const question of questions) {
+    console.log(questions);
+    questions.forEach((question) => {
+      console.log(question);
       question.click();
-      const [keyButton] = question.querySelectorAll(
-        '[data-tooltip="Answer key and points"]'
-      );
+      const [keyButton] = question.querySelectorAll(answerKeyButtonSelector);
       if (keyButton != null) {
         keyButton.click();
       }
-    }
+    });
   };
 
   clear = (start = 0, end = -1) => {
     //uncheck all boxes
-    let checked = Array.from(
-      document.getElementsByClassName(
-        'freebirdFormeditorViewAssessmentGridbodyCorrectAnswerToggle '
-      )
-    );
-
     if (end === -1) {
-      end = checked.length;
+      end = this.checkboxes.length;
     }
     // i starts from question number
     for (let i = start; i < end; i++) {
-      const ele = checked[i];
-      if (ele.classList.contains('isChecked')) {
+      const ele = this.checkboxes[i];
+      if (ele.classList.contains(checkedClass)) {
         ele.click();
       }
     }
